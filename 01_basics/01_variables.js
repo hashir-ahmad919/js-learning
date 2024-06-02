3320ef4b98a880697a30fd2db2981b91d23e827a
Hashir Ahmad Khan <101397727+hashir-ahmad919@users.noreply.github.com>
2024-06-02 18:14:33 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = 'hak47dev@google.com';
+var accountPassword = '12345'
+accountCity = "Peshawar"
+
+// accountId = 2  //not allowed
+
+
+/*
+Prefer not to use var
+because its blocked and functional scope
+*/ 
+
+console.log(accountId);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";  //treat all JS code as newer version
+
+let name = "Hashir";
+let age = 24;
+let isLoggedIn = false;
+
+// number =>   2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => stand alone video
+// undefine 
+// symbol => unique
+
+//object
+
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null); //  object
+

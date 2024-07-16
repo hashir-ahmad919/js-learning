248f2c697b4e542fce65794e1b9951ecbed3b931
Hashir Ahmad Khan <101397727+hashir-ahmad919@users.noreply.github.com>
2024-07-16 19:23:03 +0000

    change in location of file

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// ##Primitive Types
+// 7 in Number
+// String, Number, Boolean, null(empty), undefined, Symbol, BigInt
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId);
+
+
+
+// DataTypes return 
+// String => string
+// Number => Number
+// Null => object
+
+// ###Reference type/ Non Primitive Type
+// Array, Objects, Functions
+
+
+const heros = ["SuperMan", "BatMan", "SpyderMan"]
+let obj1 ={
+    name: "Hashir"
+}
+
+const myFunction = function(){
+    console.log("Hello Hashir")
+}
+
+console.log(typeof anotherId);
+
+
+// functions => function
+// array => object
+// object => object

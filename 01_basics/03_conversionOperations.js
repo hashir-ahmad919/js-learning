57a293b130acc7e962390eef6232db0799285b42
Hashir Ahmad Khan <101397727+hashir-ahmad919@users.noreply.github.com>
2024-07-16 14:54:16 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,55 @@
+let score = "33abc";
+let score1 = null;
+let score2 = undefined
+let score3 = true;
+
+
+let valueInNumber = Number(score);
+let valueInNumber1 = Number(score1);
+let valueInNumber2 = Number(score2);
+let valueInNumber3 = Number(score3);
+
+
+
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber1);
+// console.log(valueInNumber2);
+// console.log(valueInNumber3);
+
+
+// Conversions Number
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+// undefined => NaN
+
+let isLoggedIn = 1;
+let isLoggedIn1 = "";
+let isLoggedIn2 = "Hak";
+
+
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+let boolenIsLoggedIn1 = Boolean(isLoggedIn1);
+let boolenIsLoggedIn2 = Boolean(isLoggedIn2);
+
+
+// console.log(boolenIsLoggedIn);
+// console.log(boolenIsLoggedIn1);
+// console.log(boolenIsLoggedIn2);
+
+
+
+// Conversions Boolean
+// 1 => true ,  0 => false
+// "" => false, "hak" => true
+
+let someNumber = 33;
+let stringnNumber = String(someNumber);
+console.log(typeof stringnNumber);
+console.log(stringnNumber);
+
+
+
+
